a16fe6e704590a0db143e18959f05b516fa362c6
floodliu <floodliu@gmail.com>
2015-08-24 20:49:05 +0800

    1. add pace after time prediction. 2. adjust output format.

--- marathonCalculator.py
+++ marathonCalculator.py
@@ -23,10 +23,13 @@ def showPredictions(oldDist, oldTime):
 	print("\n比赛成绩预测：")
 	print("-" * 80)
 	distances = [5, 10, 21.0975, 42.195]
-	items = ["5km", "10km", "半程马拉松", "全程马拉松"]
+	items = ["5km", "10km", "Half Marathon (21.0975km)", "Full Marathon (42.195km)"]
 	for index in range(len(distances)):
-		time = predictTime(oldDist, oldTime, distances[index])
-		print(items[index] + " : " + secondsToTime(time))
+		dist = distances[index]
+		time = predictTime(oldDist, oldTime, dist)
+		secondsPerKm = int(time / dist)
+		msg = "%-30s %-20s %-20s" %(items[index], secondsToTime(time), secondsToPace(secondsPerKm) + "（分:秒/km）")
+		print(msg)
 
 def secondsToPace(seconds):
 	m = int(seconds / 60)
@@ -34,30 +37,36 @@ def secondsToPace(seconds):
 	return "%d:%02d" %(m, s)
 
 def showEasyRunPace(marathonPace):
-	easyPaceRange = [marathonPace + 38, marathonPace + 56]
-	print("轻松跑：" + secondsToPace(easyPaceRange[0]) + " ~ " + secondsToPace(easyPaceRange[1]) + "（分:秒/km）")
+	easyPace1 = marathonPace + 38
+	easyPace2 = marathonPace + 56
+	msg = "%-20s %-20s" %("Easy Run", secondsToPace(easyPace1) + "～" + secondsToPace(easyPace2) + "（分:秒/km）")
+	print(msg)
 
 def showMarathonPace(marathonPace):
-	print("配速跑：" + secondsToPace(marathonPace) + "（分:秒/km）")
+	msg = "%-20s %-20s" %("Marathon Pace", secondsToPace(marathonPace) + "（分:秒/km）")
+	print(msg)
 
 def showTempoRunPace(marathonPace):
 	tempoPace = marathonPace - 22
-	print("节奏跑：" + secondsToPace(tempoPace) + "（分:秒/km）")
+	msg = "%-20s %-20s" %("Tempo Run", secondsToPace(tempoPace) + "（分:秒/km）")
+	print(msg)
 
 def showYasso800Pace(marathonTime):
 	h = int(marathonTime / 3600)
 	m = int(marathonTime % 3600 / 60)
 	secondsPer800m = h * 60 + m
-	print("亚索800：" + secondsToPace(secondsPer800m) + "（分:秒/800m）")
+	msg = "%-20s %-20s" %("Yasso 800", secondsToPace(secondsPer800m) + "（分:秒/800m）")
+	print(msg)
 
 def showYasso1000Pace(marathonTime):
 	h = int(marathonTime / 3600)
 	m = int(marathonTime % 3600 / 60)
 	secondsPerKm = int((h * 60 + m) / 800.0 * 1000)
-	print("亚索1000：" + secondsToPace(secondsPerKm) + "（分:秒/km）")
+	msg = "%-20s %-20s" %("Yasso 1000", secondsToPace(secondsPerKm) + "（分:秒/km）")
+	print(msg)
 
 def showTrainningPaces(oldDist, oldTime):
-	print("\n马拉松训练的参考配速：")
+	print("\n全马训练的参考配速：")
 	print("-" * 80)
 	marathonDist = 42.195
 	marathonTime = predictTime(oldDist, oldTime, marathonDist)
